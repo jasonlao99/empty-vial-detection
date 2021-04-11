4ec84bfc24625ba00b7136bebbf5ea025ce46750
Rodman Zhu <rodman.y.zhu@vanderbilt.edu>
2021-04-11 14:57:16 -0500

    change resize

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 
 net =  cv2.dnn.readNetFromONNX('EVD.onnx')
 image = cv2.imread('E0000009.jpg')
-blob = cv2.dnn.blobFromImage(image, 1.0 / 255, (256, 256),(0, 0, 0), swapRB=True, crop=False)
+image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+blob = cv2.dnn.blobFromImage(image, 1.0 / 255, (256, 256),(0.485, 0.456, 0.406), swapRB=True, crop=False)
 net.setInput(blob)
 preds = net.forward()
 biggest_pred_index = np.array(preds)[0].argmax()
