7258e4244924f85856d4b0b804d83911595f5b87
Jason Lao <jason.lao@vanderbilt.edu>
2021-04-14 12:26:02 -0500

    updated main.py

--- camera/main.py
+++ camera/main.py
@@ -4,6 +4,8 @@ import os
 os.add_dll_directory(r'C:\Users\jason\Documents\Vanderbilt\4th Year Courseload\EECE Senior Design\Project\empty-vial-detection\camera')
 import ctypes as C
 import tisgrabber as IC
+from datetime import datetime
+import sys
 
 print("press space to take image, escape to end program")
 
@@ -52,7 +54,7 @@ if Camera.IsDevValid() == 1:
     print("Exposure time abs: ", ExposureTime[0])
 
     # Set an absolute exposure time, given in fractions of seconds. 0.0303 is 1/30 second:
-    Camera.SetPropertyAbsoluteValue("Exposure", "Value", 0.004)
+    Camera.SetPropertyAbsoluteValue("Exposure", "Value", 0.002)
 
     # Proceed with Gain, since we have gain automatic, disable first. Then set values.
     Gainauto = [0]
@@ -82,7 +84,7 @@ if Camera.IsDevValid() == 1:
     Camera.GetPropertySwitch("Zoom", "Auto", WhiteBalanceAuto)
     print("Zoom auto : ", WhiteBalanceAuto[0])
 
-    Camera.SetPropertyValue("Zoom", "Value", 69)
+    Camera.SetPropertyValue("Zoom", "Value", 50)
 
 
     Focusauto = [0]
@@ -91,11 +93,20 @@ if Camera.IsDevValid() == 1:
     Camera.GetPropertySwitch("Focus", "Auto", Focusauto)
     print("Focus auto : ", Focusauto[0])
 
-    Camera.SetPropertyValue("Focus", "Value", 331)
+    Camera.SetPropertyValue("Focus", "Value", 270)
 
     img_counter = 0
 
+    correct = 0
+    classes = ['empty', 'not empty']
+
     net = cv.dnn.readNetFromONNX('EVD.onnx')
+
+    original_stdout = sys.stdout
+    BottleID = 0
+
+    f = open('output.txt', 'w')
+    f.write("Date \t \t Timestamp \t BottleID \t Prediction \t Prediction Level \n")
 
     while True:
         # Snap an image
@@ -118,19 +129,33 @@ if Camera.IsDevValid() == 1:
 
             image = frame
             image = cv.resize(image, (256, 256), interpolation=cv.INTER_AREA)
-            blob = cv.dnn.blobFromImage(image, 1.0 / 255, (256, 256), (0.485, 0.456, 0.406), swapRB=True, crop=False)
+            blob = cv.dnn.blobFromImage(image, 1.0/255, (256, 256), (0.485, 0.456, 0.406), swapRB=True, crop=False)
             net.setInput(blob)
             preds = net.forward()
-            biggest_pred_index = np.array(preds)[0].argmax()
+            biggest_pred_index = np.array(preds)[0].argmax(0)
+            # threshold = 2.5
+            # idx = np.argsort(preds[0])[::-1][0]
+            # scuff_conf = (preds[0][idx]/threshold) * 100
+            # text = "Label: {}, {}".format(classes[idx], scuff_conf)
+            now = datetime.now()
+            date = now.strftime("%m/%d/%Y")
+            timestamp = now.strftime("%H: %M: %S")
+            s = "{} \t {} \t {} \t\t {} \t {:.6f} \n".format(date, timestamp, BottleID, classes[biggest_pred_index], np.array(preds)[0][biggest_pred_index])
+            f.write(s)
 
-            if biggest_pred_index == 1:
-                print("Predicted class: not empty")
-            else:
-                print("Predicted class: empty")
+            BottleID += 1
+
+            # print(str(text))
+            # print(text)
+            #if biggest_pred_index == 1:
+            #    print("Predicted class: not empty")
+            #else:
+            #    print("Predicted class: empty")
 
 
     Camera.StopLive()
     cv.destroyWindow('Window')
+    f.close()
 
 
 else:
